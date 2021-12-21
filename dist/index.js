ed7f7a6a002596c05a43bd5ab6afd637d362e751
Bogdan Plieshka <bogdan.plieshka@zattoo.com>
2021-12-21 20:27:35 +0100

    ...

--- dist/index.js
+++ dist/index.js
@@ -6504,7 +6504,7 @@ const getOutput = (changes, domains) => {
     const token = core.getInput('token', {required: true});
     const octokit = github.getOctokit(token);
 
-    const response = octokit.rest.repos.getCommit({
+    const response = await octokit.rest.repos.getCommit({
         ...github.context.repo,
         ref: github.context.ref,
     });
