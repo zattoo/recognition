aea329ca430d5118f9b858201982e2be04a110ec
Bogdan Plieshka <bogdan.plieshka@zattoo.com>
2021-12-22 10:12:50 +0100

    Test

--- src/index.js
+++ src/index.js
@@ -28,6 +28,8 @@ const getOutput = (changes, domains) => {
     const token = core.getInput('token', {required: true});
     const octokit = github.getOctokit(token);
 
+    console.log('github.context', github.context);
+
     let files = [];
 
     if (pull_request) {
